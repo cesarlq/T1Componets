75995886d4fb7959053a8d0cb8ca00fe6f1776c9
cesarlq <cesar.lopez@t1paginas.com>
2025-05-27 01:57:51 -0600

    feat: add LayoutProvider export to index.d.ts

--- dist/lib/types/index.d.ts
+++ dist/lib/types/index.d.ts
@@ -22,3 +22,4 @@ export { default as Cards } from './Components/Cards';
 export { default as T1Icon } from './Components/T1Icon';
 export { Navbar } from './Components/MenuComponent/Navbar';
 export { Sidebar } from './Components/MenuComponent/Sidebar';
+export { LayoutProvider } from './Components/MenuComponent/LayoutProvider';
